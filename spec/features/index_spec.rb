50116ecb6cd7283d95020dddbfc0c19fc381ada2
MikaelFeher <mikael.feher@gmail.com>
2016-07-05 15:47:43 +0200

    added data-folder with a yaml-file, a partials-folder with a footer and refactored the layout.html into a haml-file

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -17,4 +17,22 @@ describe 'index', type: :feature do
   it 'shows an image' do
     expect(page).to have_css 'img[src="/images/middleman-logo.svg"]'
   end
+
+  it 'displays project list' do
+    expect(page).to have_css '.projects'
+    within '.projects' do
+      expect(page).to have_content 'My First Website'
+      expect(page).to have_content 'FizzBuzz'
+    end
+  end
+
+  it 'renders footer partial' do
+    expect(page).to have_selector 'footer'
+    within 'footer' do
+      expect(page).to have_content 'My Portfolio'
+      expect(page).to have_content 'Built with the awsome Middleman framework'
+    end
+  end
+
+
 end
